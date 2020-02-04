334f681f8ef4ca427256346e7a97c1c00da89c6e
toshirohug <60601174+toshirohug@users.noreply.github.com>
2020-02-04 08:06:16 -0300

    1st commit

---
+++ api_test/features/support/env.rb
@@ -0,0 +1,54 @@
+require 'capybara'
+require 'capybara/cucumber'
+require 'selenium-webdriver'
+require 'site_prism'
+require 'rspec'
+require 'rspec/retry'
+require 'money'
+require 'csv'
+require_relative '../framework/helper.rb'
+require_relative 'page_helper.rb'
+
+World(Pages)
+World(Helper)
+
+Money.locale_backend = :i18n
+I18n.enforce_available_locales = false
+
+$total = nil # vai armazenar o total do produto no site.
+ENVIRONMENT_TYPE = ENV['ENVIRONMENT_TYPE']
+HEADLESS = ENV['HEADLESS']
+
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/config/#{ENVIRONMENT_TYPE}.yml")
+
+## register driver according with browser chosen
+Capybara.register_driver :selenium do |app|
+ 
+  if HEADLESS.eql?('headless')
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: {browser: 'ALL'})
+    option = ::Selenium::WebDriver::Chrome::Options.new(args: ['--headless', '--disable-gpu', '--start-maximized'])
+    session = Capybara::Selenium::Driver.new(app, browser: :chrome, options: option, desired_capabilities: caps)
+  elsif HEADLESS.eql?('no_headless')
+    caps = Selenium::WebDriver::Remote::Capabilities.chrome(loggingPrefs: {browser: 'ALL'})
+    option = ::Selenium::WebDriver::Chrome::Options.new(args: ['--disable-infobars', '--start-maximized'])
+    session = Capybara::Selenium::Driver.new(app, browser: :chrome, options: option, desired_capabilities: caps)
+  end
+  
+end
+
+Capybara.configure do |config|
+  config.default_driver = :selenium
+  config.app_host = CONFIG['url_default']
+  config.default_max_wait_time = 30
+end
+
+SitePrism.configure do |config|
+  config.use_implicit_waits = true
+end
+
+# tratamento de erro.
+RSpec.configure do |config|
+  config.verbose_retry = true
+  config.default_retry_count = 3
+  config.exceptions_to_retry = [Net::ReadTimeout]
+end
---
+++ api_test/features/support/hooks.rb
@@ -0,0 +1,17 @@
+require_relative '../framework/helper.rb'
+
+Before do |scenario|
+  include Capybara::DSL
+end
+
+After do |scenario|
+  scenario_name = scenario.name.gsub(/[^A-Za-z ]/, '').gsub(/\s+/, '_')
+
+  if scenario.failed?
+    take_screenshot(file_name: scenario_name.downcase!, test_result: 'failed')
+    ##add_browser_logs##
+  else
+    take_screenshot(file_name: scenario_name.downcase!, test_result: 'passed')
+  end
+end
+
